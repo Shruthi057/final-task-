3276a857993124f0c2dfd8703aa91a8527392172
Shruthi L <shruthi057@gmail.com>
2025-10-29 14:06:55 +0530

    updated code

--- src/components/SampleComponent.js
+++ src/components/SampleComponent.js
@@ -44,6 +44,30 @@ const SampleComponent = () => {
             <p className='text'>{item.text}</p>
           </div>
         ))}
+        
+        {/* Dotted connecting lines */}
+        <div className="connecting-lines">
+          <svg className="line-svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+            {/* Horizontal line from item 1 to item 2 */}
+            <path 
+              d="M 25 50 L 75 50" 
+              className="dotted-line line-1"
+              strokeDasharray="4,4"
+            />
+            {/* Vertical line from item 2 to item 3 */}
+            <path 
+              d="M 75 50 L 75 75" 
+              className="dotted-line line-2"
+              strokeDasharray="4,4"
+            />
+            {/* Horizontal line from item 3 to item 4 */}
+            <path 
+              d="M 75 75 L 25 75" 
+              className="dotted-line line-3"
+              strokeDasharray="4,4"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   );
